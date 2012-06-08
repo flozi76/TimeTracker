b93ee6f9a6886cd8585638a0ef28a47a2417f1ef
Florian Zimmermann <florian.zimmermann@bbv.ch>
2012-06-08 12:17:56 +0200

    Some Tests ... not working yet

---
+++ Source/TimeTracker/TimeTracker.Test/GravitySensorServiceFixture.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TimeTracker.Test
+{
+    using System;
+    using Xunit;
+
+    public class GravitySensorServiceFixture : IDisposable
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            Console.WriteLine("Some Test????");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
