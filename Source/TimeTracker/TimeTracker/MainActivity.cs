09f500bae3cd83d8bf289ce2e002dc00127bb527
Florian Zimmermann <flozi76@gmail.com>
2012-06-10 11:02:32 +0200

    Added Listenservice

--- Source/TimeTracker/TimeTracker/MainActivity.cs
+++ Source/TimeTracker/TimeTracker/MainActivity.cs
@@ -10,6 +10,7 @@ namespace TimeTracker
 {
     using Android.Content;
     using Android.Locations;
+    using Android.Util;
     using TimeTracker.Core.BusinessLayer;
     using TimeTracker.Core.Domain;
     using TimeTracker.Core.Geo;
@@ -19,6 +20,8 @@ namespace TimeTracker
     {
         private Button addCurrentLocationButton;
         private TextView listLocationsText;
+        private Button startListenServiceButton;
+        private Button stopListenServiceButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -26,8 +29,7 @@ namespace TimeTracker
 
             SetContentView(Resource.Layout.Main);
 
-            this.listLocationsText = FindViewById<TextView>(Resource.Id.ListLocations);
-            this.addCurrentLocationButton = FindViewById<Button>(Resource.Id.btnTrackcCurrentLocation);
+            this.BindElements();
 
             var trackLocations = CentralStation.Instance.Ainject.ResolveType<ITrackLocationManager>().GetTrackLocations();
             var stringBuilderList = new StringBuilder();
@@ -39,96 +41,38 @@ namespace TimeTracker
 
             this.listLocationsText.Text = stringBuilderList.ToString();
 
+            this.RegisterActions();
+
+        }
+
+        private void RegisterActions()
+        {
             this.addCurrentLocationButton.Click += delegate
                                                        {
                                                            var intent = new Intent(this, typeof(SelectLocationActivity));
                                                            StartActivity(intent);
                                                        };
 
-            //this.stringBuilder = new StringBuilder();
-            //var geocoder = new Geocoder(this);
-            //this.locationManager = (LocationManager)GetSystemService(LocationService);
+            this.startListenServiceButton.Click += delegate
+                                                       {
+                                                           var intent = new Intent(this, typeof(TimeTrackerService));
+                                                           this.StartService(intent);
+                                                       };
 
-
-            //var criteria = new Criteria
-            //                   {
-            //                       Accuracy = Accuracy.Coarse,
-            //                       PowerRequirement = Power.Low,
-            //                   };
-            //string bestProvider = this.locationManager.GetBestProvider(criteria, true);
-
-            //Location lastKnownLocation = this.locationManager.GetLastKnownLocation(bestProvider);
-
-            //if (lastKnownLocation != null)
-            //{
-            //    this.currentLocation = new TrackLocation { Latitude = lastKnownLocation.Latitude, Longitude = lastKnownLocation.Longitude };
-            //    this.stringBuilder.AppendLine(string.Format("Last known location, lat: {0}, long: {1}", lastKnownLocation.Latitude, lastKnownLocation.Longitude));
-            //    this.UpdateAddress();
-            //    this.locationText.Text = this.stringBuilder.ToString();
-            //}
-
-            //this.locationManager.RequestLocationUpdates(bestProvider, 1000, 1, this);
+            this.stopListenServiceButton.Click += delegate
+            {
+                var intent = new Intent(this, typeof(TimeTrackerService));
+                this.StopService(intent);
+            };
         }
 
-        //public void OnLocationChanged(Location location)
-        //{
-        //    this.stringBuilder.AppendLine(string.Format("Location updated, lat: {0}, long: {1}", location.Latitude, location.Longitude));
-
-        //    if (this.currentLocation != null)
-        //    {
-        //        var newCoordinate = new Coordinate { Latitude = location.Latitude, Longitude = location.Longitude };
-        //        var lastCoordinate = new Coordinate { Latitude = this.currentLocation.Latitude, Longitude = this.currentLocation.Longitude };
-        //        var distance = this.distanceCalculator.Distance(lastCoordinate, newCoordinate, UnitsOfLength.Meter);
-        //        this.stringBuilder.AppendLine(string.Format("Distance to last coordinate: {0}", distance));
-        //    }
-
-        //    this.currentLocation = new TrackLocation
-        //    {
-        //        Longitude = location.Longitude,
-        //        Latitude = location.Latitude
-        //    };
-
-        //    this.UpdateAddress();
-
-        //    this.locationText.Text = this.stringBuilder.ToString();
-        //}
-
-        //private void UpdateAddress()
-        //{
-        //    try
-        //    {
-        //        Address address = this.geocoder.GetFromLocation(this.currentLocation.Latitude, this.currentLocation.Longitude, 1).FirstOrDefault();
-
-        //        if (address != null)
-        //        {
-        //            this.stringBuilder.AppendLine(string.Format("Country: {0}-{1}", address.CountryCode, address.CountryName));
-        //            this.stringBuilder.AppendLine(string.Format("City: {0}-{1}", address.PostalCode, address.Locality));
-        //            this.stringBuilder.AppendLine(string.Format("Street: {0}-{1}", address.Thoroughfare, address.FeatureName));
-
-        //            this.currentLocation.City = address.Locality;
-        //            this.currentLocation.PostalCode = address.PostalCode;
-        //            this.currentLocation.Country = address.CountryName;
-        //            this.currentLocation.HouseNumber = address.FeatureName;
-        //            this.currentLocation.Street = address.Thoroughfare;
-        //        }
-        //    }
-        //    catch (IOException io)
-        //    {
-        //        Android.Util.Log.Debug("LocationActivity", io.Message);
-        //    }
-        //}
-
-        //public void OnProviderDisabled(string provider)
-        //{
-        //}
-
-        //public void OnProviderEnabled(string provider)
-        //{
-        //}
-
-        //public void OnStatusChanged(string provider, Availability status, Bundle extras)
-        //{
-        //}
+        private void BindElements()
+        {
+            this.listLocationsText = FindViewById<TextView>(Resource.Id.ListLocations);
+            this.addCurrentLocationButton = FindViewById<Button>(Resource.Id.btnTrackcCurrentLocation);
+            this.startListenServiceButton = FindViewById<Button>(Resource.Id.btnStartLocationListener);
+            this.stopListenServiceButton = FindViewById<Button>(Resource.Id.btnStopLocationListener);
+        }
     }
 }
 
---
+++ Source/TimeTracker/TimeTracker/TimeTrackerService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Locations;
+    using Android.OS;
+    using Android.Util;
+
+    [Service]
+    public class TimeTrackerService : Service, ILocationListener
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            Log.Warn("SimpleService", "On bind");
+            throw new NotImplementedException();
+        }
+
+        System.Threading.Timer timer;
+
+        public override void OnStart(Android.Content.Intent intent, int startId)
+        {
+
+            base.OnStart(intent, startId);
+            Log.Warn("SimpleService", "SimpleService started");
+            DoStuff();
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            this.timer.Dispose();
+            Log.Warn("SimpleService", "SimpleService stopped");
+        }
+
+        public void DoStuff()
+        {
+            this.timer = new System.Threading.Timer((o) =>
+                this.PeriodCheck(), null, 0, 4000);
+        }
+
+        private void PeriodCheck()
+        {
+            Log.Debug("TimeTrackerService", "hello from simple service");
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+        }
+    }
+}
